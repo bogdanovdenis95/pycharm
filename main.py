4ff0279f9e55254dd2cd289e796f5a6437037c2f
Your Name <you@example.com>
2024-01-15 17:08:34 +0300

    Added main.py and imported functions

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from funcs import add_numbers, subtract_numbers
+
+
+result_sum = add_numbers(10, 5)
+result_diff = subtract_numbers(10, 5)
+
+
+print("Sum:", result_sum)
+print("Difference:", result_diff)
